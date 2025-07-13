82ec1bd27aa073c22b6fed6054a6518555a32a00
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-13 19:14:36 -0400

    fix plugin import and registration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,9 @@
 import { HardhatUserConfig } from 'hardhat/config';
-
-import '@nomicfoundation/hardhat-toolbox';
-
-import './src/plugin';
+import HardhatLinearization from './src/plugin.js'
 
 export default <HardhatUserConfig> {
   solidity: '0.8.9',
+  plugins:[HardhatLinearization],
   typechain: {
     dontOverrideCompile: true,
   },
