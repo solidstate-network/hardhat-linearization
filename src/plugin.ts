c5f5f4d8868d270347937b06841b8b8905ae06a9
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-13 19:17:48 -0400

    move task action to separate file

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,39 +1,10 @@
 import { task } from 'hardhat/config';
-import { isFullyQualifiedName, parseFullyQualifiedName, getFullyQualifiedName } from 'hardhat/utils/contract-names';
-import { astDereferencer, findAll } from 'solidity-ast/utils.js';
 import { HardhatPlugin } from 'hardhat/types/plugins';
-import { readJsonFile } from '@nomicfoundation/hardhat-utils/fs';
 
 const taskPrintLinearization = task("print-linearization")
   .addPositionalArgument({ name: "contract", description: "The contract name or fully qualified name (contracts/File.sol:ContractName)"})
   .addFlag({ name: "noCompile", description: "Skip compilation"})
-  .setAction(async (args: { contract: string, noCompile: boolean }, hre) => {
-    if (!args.noCompile) {
-      await hre.tasks.getTask('compile').run({ quiet: true });
-    }
-    let fullName, sourceName, contractName;
-    if (isFullyQualifiedName(args.contract)) {
-      fullName = args.contract;
-      ({ sourceName, contractName } = parseFullyQualifiedName(args.contract));
-    } else {
-      ({ sourceName, contractName } = await hre.artifacts.readArtifact(args.contract));
-      fullName = getFullyQualifiedName(sourceName, contractName);
-    }
-    const buildInfoId = await hre.artifacts.getBuildInfoId(fullName)
-    const buildInfoPath = await hre.artifacts.getBuildInfoOutputPath(buildInfoId!);
-    const buildInfo = await readJsonFile(buildInfoPath!);
-    if (buildInfo === undefined) {  
-      throw new Error('Build info not found');
-    }
-    const deref = astDereferencer(buildInfo.output);
-    for (const c of findAll('ContractDefinition', buildInfo.output.sources[`project/${sourceName}`]!.ast)) {
-      if (c.name === contractName) {
-        const list = c.linearizedBaseContracts.map(id => deref('ContractDefinition', id).name);
-        console.log(list.join('\n'));
-        break;
-      }
-    }
-  }).build();
+  .setAction(import.meta.resolve('./task-action.js')).build();
 
 const plugin: HardhatPlugin = {
   id: 'hardhat-linearization',
---
+++ src/task-action.ts
@@ -0,0 +1,42 @@
+import { isFullyQualifiedName, parseFullyQualifiedName, getFullyQualifiedName } from 'hardhat/utils/contract-names';
+import { astDereferencer, findAll } from 'solidity-ast/utils.js';
+import { readJsonFile } from '@nomicfoundation/hardhat-utils/fs';
+import { NewTaskActionFunction } from "hardhat/types/tasks";
+
+interface TaskActionArguments {
+  contract: string;
+  noCompile: boolean;
+}
+
+const action: NewTaskActionFunction<TaskActionArguments> = async (
+  args,
+  hre,
+) => {
+  if (!args.noCompile) {
+    await hre.tasks.getTask('compile').run({ quiet: true });
+  }
+  let fullName, sourceName, contractName;
+  if (isFullyQualifiedName(args.contract)) {
+    fullName = args.contract;
+    ({ sourceName, contractName } = parseFullyQualifiedName(args.contract));
+  } else {
+    ({ sourceName, contractName } = await hre.artifacts.readArtifact(args.contract));
+    fullName = getFullyQualifiedName(sourceName, contractName);
+  }
+  const buildInfoId = await hre.artifacts.getBuildInfoId(fullName)
+  const buildInfoPath = await hre.artifacts.getBuildInfoOutputPath(buildInfoId!);
+  const buildInfo = await readJsonFile(buildInfoPath!);
+  if (buildInfo === undefined) {  
+    throw new Error('Build info not found');
+  }
+  const deref = astDereferencer(buildInfo.output);
+  for (const c of findAll('ContractDefinition', buildInfo.output.sources[`project/${sourceName}`]!.ast)) {
+    if (c.name === contractName) {
+      const list = c.linearizedBaseContracts.map(id => deref('ContractDefinition', id).name);
+      console.log(list.join('\n'));
+      break;
+    }
+  }
+};
+
+export default action;
