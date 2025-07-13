6b1ec2e8b43c14884979db6df8758036440ca20c
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-13 19:14:16 -0400

    update task

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,15 @@
 import { task } from 'hardhat/config';
 import { isFullyQualifiedName, parseFullyQualifiedName, getFullyQualifiedName } from 'hardhat/utils/contract-names';
-import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
-import { astDereferencer, findAll } from 'solidity-ast/utils';
+import { astDereferencer, findAll } from 'solidity-ast/utils.js';
+import { HardhatPlugin } from 'hardhat/types/plugins';
+import { readJsonFile } from '@nomicfoundation/hardhat-utils/fs';
 
-task("print-linearization")
-  .addPositionalParam("contract", "The contract name or fully qualified name (contracts/File.sol:ContractName)")
-  .addFlag("noCompile", "Skip compilation")
-  .setAction(async (args: { contract: string, noCompile: boolean }, hre, runSuper) => {
+const taskPrintLinearization = task("print-linearization")
+  .addPositionalArgument({ name: "contract", description: "The contract name or fully qualified name (contracts/File.sol:ContractName)"})
+  .addFlag({ name: "noCompile", description: "Skip compilation"})
+  .setAction(async (args: { contract: string, noCompile: boolean }, hre) => {
     if (!args.noCompile) {
-      await hre.run(TASK_COMPILE, { quiet: true });
+      await hre.tasks.getTask('compile').run({ quiet: true });
     }
     let fullName, sourceName, contractName;
     if (isFullyQualifiedName(args.contract)) {
@@ -18,16 +19,25 @@ task("print-linearization")
       ({ sourceName, contractName } = await hre.artifacts.readArtifact(args.contract));
       fullName = getFullyQualifiedName(sourceName, contractName);
     }
-    const buildInfo = await hre.artifacts.getBuildInfo(fullName);
-    if (buildInfo === undefined) {
+    const buildInfoId = await hre.artifacts.getBuildInfoId(fullName)
+    const buildInfoPath = await hre.artifacts.getBuildInfoOutputPath(buildInfoId!);
+    const buildInfo = await readJsonFile(buildInfoPath!);
+    if (buildInfo === undefined) {  
       throw new Error('Build info not found');
     }
     const deref = astDereferencer(buildInfo.output);
-    for (const c of findAll('ContractDefinition', buildInfo.output.sources[sourceName]!.ast)) {
+    for (const c of findAll('ContractDefinition', buildInfo.output.sources[`project/${sourceName}`]!.ast)) {
       if (c.name === contractName) {
         const list = c.linearizedBaseContracts.map(id => deref('ContractDefinition', id).name);
         console.log(list.join('\n'));
         break;
       }
     }
-  });
+  }).build();
+
+const plugin: HardhatPlugin = {
+  id: 'hardhat-linearization',
+  tasks: [taskPrintLinearization]
+}
+
+export default plugin;
