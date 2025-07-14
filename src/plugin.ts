8fb674596d744bff3f05f6caad2af91dd2b12d9c
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-14 11:36:12 -0400

    run prettier and knip on commit

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,19 @@
 import { task } from 'hardhat/config';
 import { HardhatPlugin } from 'hardhat/types/plugins';
 
-const taskPrintLinearization = task("print-linearization")
-  .addPositionalArgument({ name: "contract", description: "The contract name or fully qualified name (contracts/File.sol:ContractName)"})
-  .addFlag({ name: "noCompile", description: "Skip compilation"})
-  .setAction(import.meta.resolve('./task-action.js')).build();
+const taskPrintLinearization = task('print-linearization')
+  .addPositionalArgument({
+    name: 'contract',
+    description:
+      'The contract name or fully qualified name (contracts/File.sol:ContractName)',
+  })
+  .addFlag({ name: 'noCompile', description: 'Skip compilation' })
+  .setAction(import.meta.resolve('./task-action.js'))
+  .build();
 
 const plugin: HardhatPlugin = {
   id: 'hardhat-linearization',
-  tasks: [taskPrintLinearization]
-}
+  tasks: [taskPrintLinearization],
+};
 
 export default plugin;
--- src/task-action.ts
+++ src/task-action.ts
@@ -1,7 +1,11 @@
-import { isFullyQualifiedName, parseFullyQualifiedName, getFullyQualifiedName } from 'hardhat/utils/contract-names';
-import { astDereferencer, findAll } from 'solidity-ast/utils.js';
 import { readJsonFile } from '@nomicfoundation/hardhat-utils/fs';
-import { NewTaskActionFunction } from "hardhat/types/tasks";
+import { NewTaskActionFunction } from 'hardhat/types/tasks';
+import {
+  isFullyQualifiedName,
+  parseFullyQualifiedName,
+  getFullyQualifiedName,
+} from 'hardhat/utils/contract-names';
+import { astDereferencer, findAll } from 'solidity-ast/utils.js';
 
 interface TaskActionArguments {
   contract: string;
@@ -20,19 +24,28 @@ const action: NewTaskActionFunction<TaskActionArguments> = async (
     fullName = args.contract;
     ({ sourceName, contractName } = parseFullyQualifiedName(args.contract));
   } else {
-    ({ sourceName, contractName } = await hre.artifacts.readArtifact(args.contract));
+    ({ sourceName, contractName } = await hre.artifacts.readArtifact(
+      args.contract,
+    ));
     fullName = getFullyQualifiedName(sourceName, contractName);
   }
-  const buildInfoId = await hre.artifacts.getBuildInfoId(fullName)
-  const buildInfoPath = await hre.artifacts.getBuildInfoOutputPath(buildInfoId!);
+  const buildInfoId = await hre.artifacts.getBuildInfoId(fullName);
+  const buildInfoPath = await hre.artifacts.getBuildInfoOutputPath(
+    buildInfoId!,
+  );
   const buildInfo = await readJsonFile(buildInfoPath!);
-  if (buildInfo === undefined) {  
+  if (buildInfo === undefined) {
     throw new Error('Build info not found');
   }
   const deref = astDereferencer(buildInfo.output);
-  for (const c of findAll('ContractDefinition', buildInfo.output.sources[`project/${sourceName}`]!.ast)) {
+  for (const c of findAll(
+    'ContractDefinition',
+    buildInfo.output.sources[`project/${sourceName}`]!.ast,
+  )) {
     if (c.name === contractName) {
-      const list = c.linearizedBaseContracts.map(id => deref('ContractDefinition', id).name);
+      const list = c.linearizedBaseContracts.map(
+        (id) => deref('ContractDefinition', id).name,
+      );
       console.log(list.join('\n'));
       break;
     }
