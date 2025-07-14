2933813ad17eb8180d953c95e64049caded31911
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-14 19:17:31 -0400

    use hardhat-solidstate-utils dependency

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import { HardhatPlugin } from 'hardhat/types/plugins';
 
 const plugin: HardhatPlugin = {
   id: 'hardhat-linearization',
+  dependencies: [
+    async () => {
+      const { default: HardhatSolidstateUtils } = await import(
+        '@solidstate/hardhat-solidstate-utils'
+      );
+      return HardhatSolidstateUtils;
+    },
+  ],
   tasks: [taskPrintLinearization],
 };
 
