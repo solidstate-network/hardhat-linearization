36c8b7a5af886030b377ac9cac7573c9e22f905d
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-14 13:12:06 -0400

    remove typechain config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,6 @@ import { HardhatUserConfig } from 'hardhat/config';
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   plugins: [HardhatLinearization],
-  typechain: {
-    dontOverrideCompile: true,
-  },
 };
 
 export default config;
