28ee2958905157d520f25b3f0b3f1778139af19c
Nick Barry <itsnickbarry@protonmail.ch>
2025-07-16 22:28:08 -0400

    use solidstate contracts import to replace test contracts, add knip config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,12 @@ import HardhatLinearization from './src/index.js';
 import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9',
+  solidity: {
+    version: '0.8.30',
+    dependenciesToCompile: [
+      '@solidstate/contracts/token/fungible/FungibleToken.sol',
+    ],
+  },
   plugins: [HardhatLinearization],
 };
 
